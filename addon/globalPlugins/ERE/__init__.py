aaa88cdb00a3e544b05676172c7deae08e9e76a8
Kazto Kitabatake <kitabatake1013@outlook.jp>
2022-07-12 20:41:41 +0900

    add updater

--- addon/globalPlugins/ERE/__init__.py
+++ addon/globalPlugins/ERE/__init__.py
@@ -2,9 +2,11 @@
 
 from __future__ import unicode_literals
 import addonHandler, globalPluginHandler, globalVars, threading, time, wx
+import speech
 from logHandler import log
 from .constants import *
 from . import updater
+from ._englishToKanaConverter.englishToKanaConverter import EnglishToKanaConverter
 
 try:
 	import addonHandler
@@ -12,12 +14,31 @@ try:
 except:
 	_ = lambda x : x
 
+
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self):
 		super(globalPluginHandler.GlobalPlugin, self).__init__()
 		self.autoUpdateChecker = updater.AutoUpdateChecker()
 		self.autoUpdateChecker.autoUpdateCheck()
+		self._setup()
 
 	def terminate(self):
 		super(GlobalPlugin, self).terminate()
 		self.autoUpdateChecker.terminate()
+
+	def _setup(self):
+		if hasattr(speech, "speech"):
+			processText_original = speech.speech.processText
+		else:
+			processText_original = speech.processText
+		c = EnglishToKanaConverter()
+
+		def processText(locale, text, symbolLevel):
+			text = processText_original(locale, text, symbolLevel)
+			if locale.startswith("ja"):
+				text = c.process(text)
+			return text
+		if hasattr(speech, "speech"):
+			speech.speech.processText = processText
+		else:
+			speech.processText = processText
