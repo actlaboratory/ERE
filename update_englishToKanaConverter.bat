00c2ecfcbd695fd3cf04126f4509a95fb3430eb9
Kazto Kitabatake <kitabatake1013@outlook.jp>
2025-12-02 20:58:45 +0900

    サブモジュールの更新時にgitのメッセージが表示されないように修正

--- update_englishToKanaConverter.bat
+++ update_englishToKanaConverter.bat
@@ -1,8 +1,8 @@
 @echo off
 cd %~dp0
 cd addon\globalPlugins\ERE\_englishToKanaConverter 
-git checkout main
-git pull
+git checkout -q main
+git pull -q
 cd ..\..\..\..
 git add addon\globalPlugins\ERE\_englishToKanaConverter 
 git commit -m "update submodule"
